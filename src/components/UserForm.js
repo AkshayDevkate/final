2ce2c5ce13a32c30ddb920e481977150d98ca426
Akshay Devkate <devkatte.akshay98@gmail.com>
2022-03-15 15:40:38 +0100

    Testing workflow

---
+++ src/components/UserForm.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+
+export class UserForm extends Component {
+render() {
+    return (
+        <h1> We are at h1</h1>
+    );
+           
+}
+}
+export default UserForm;
