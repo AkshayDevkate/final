aab52a8b57e176728226c9d729905bb6a3e4ca60
Akshay Devkate <devkatte.akshay98@gmail.com>
2022-03-15 19:35:16 +0100

    Try 4  without browser router

--- src/components/FormSteps/FirstStep.js
+++ src/components/FormSteps/FirstStep.js
@@ -1,19 +1,20 @@
-import { render } from '@testing-library/react';
+
 import React, { Component } from 'react';
 
-
 export class FirstStep extends Component {
-
-
-render () {
  
-    return (
-        <>
-        <h1>Hello world </h1>
-        </>
-        );
+    render () {
+
+        return (
+       
+            <h1>Hello world </h1>
+           
+            );
+
+    }
+
  
-}
+
 }
 
 export default FirstStep;
