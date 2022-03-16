c611f65c24d810cda5ddfe1562078c7d3ead2768
Akshay Devkate <devkatte.akshay98@gmail.com>
2022-03-16 13:28:25 +0100

    Try 3 final

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 // componets 
 import Main from './components/Main';
 import './App.css';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <>
+    <Heather />
        <Main />
-    </>
+       <Footer/>
+     </>
 
   );
 }
